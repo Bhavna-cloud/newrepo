3ae971126d076d40920f46224e9995f0ac320999
Bhavna <bhavna.rani199@gmail.com>
2025-01-30 06:13:25 +0530

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Navigate to the application directory
 cd /home/ubuntu
-npm -f install
+
+# Install the required NPM dependencies
+npm install
+
+# Confirm that the dependencies were installed
+echo "NPM install completed successfully"
