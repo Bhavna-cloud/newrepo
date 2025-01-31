89e9b5b5cf40a3714b74d60b92b6b55df0f96afe
Bhavna <bhavna.rani199@gmail.com>
2025-01-31 18:04:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const os = require('os');  // To get the instance hostname
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
 
-const PORT = 5500;
+const PORT = process.env.PORT || 3000
 
 http.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
+    console.log(Listening on port ${PORT})
+})
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-    // Log the hostname of the instance handling the request
-    const instanceId = os.hostname();  // Get the hostname (unique to the instance)
-    console.log(`Request handled by: ${instanceId}`);  // Log to console
+    res.sendFile(__dirname + '/index.html')
+})
 
-    res.sendFile(__dirname + '/index.html');  // Serve the HTML file
-});
-
-// Socket.IO setup
-const io = require('socket.io')(http);
+// Socket 
+const io = require('socket.io')(http)
 
 io.on('connection', (socket) => {
-    console.log('Connected...');
+    console.log('Connected...')
     socket.on('message', (msg) => {
-        socket.broadcast.emit('message', msg);
-    });
-});
+        socket.broadcast.emit('message', msg)
+    })
+
+})
